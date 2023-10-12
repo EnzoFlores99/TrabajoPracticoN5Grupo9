7639da0b0af8ffa36e230690bb30d9f2d44ba2e8
salvavelazquez <augustorvelazquez@gmail.com>
2023-10-12 00:44:32 -0300

    Se agrego parte de la escena 2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import winner from "./escenas/winner.js"
+import Escena1 from "./escenas/Escena1.js";
+import Escena2 from "./escenas/Escena2.js";
+
 let config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT, // Escala para ajustar el juego a la ventana
+        autoCenter: Phaser.Scale.CENTER_BOTH // Centra automáticamente el juego en la ventana
+    },
     physics:{
         default: 'arcade',
         arcade: {
-            gravity: {y:300},
+            gravity: {y:0},
             debug: false
         }
     },
-    scene:[winner]
+    scene:[Escena1, Escena2]
 };
 let game = new Phaser.Game(config);
