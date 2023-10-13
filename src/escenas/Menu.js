ce12f3bc3db52fb38f9d4633d785495694dc217d
maxi condori <profekennedy5040@gmail.com>
2023-10-13 01:15:39 -0300

    se termino el juegazooooo·sin luz papa

--- src/escenas/Menu.js
+++ src/escenas/Menu.js
@@ -1,79 +1,79 @@
-class Menu  extends Phaser.Scene{
-   
-    constructor(){
-              super(" menu");
-              
-    }
-     
-    preload(){
-       // en priload vamos a cargar nuestras imagenes que vamos a usar 
-       // para tenerlo disponibles para que la paguna luego la pueda renderizar
-       this.load.audio('musicafondo',  '/public/img/musicafondo.mp3');
-       this.load.image('start', '/public/img/start.png');
-       this.load.image('menunave', '/public/img/fondomenunave.jpg');
-       this.load.image('titulo', '/public/img/titulo.png');
-       this.load.image('musicaon', '/public/img/botonsonidoactivo.png');
-       this.load.image('musicaoff', '/public/img/botonsonidodesactivado.png');
-      
-       
+class Menu extends Phaser.Scene {
 
-    }
+  constructor() {
+    super("menu");
 
-    create(){
-      ////////////////////////////////////////////////////////
-       this.add.image(400,300,'menunave');  // imagen del fondo 
-       this.add.image(400,200,'titulo').setScale(0.4);
-       
-
-        this.starbutton   = this.add.image(400, 500,'start').setScale(0.7).setInteractive();
-            
-              this.starbutton.on('pointerdown', () =>{
-                this.scene.start('inicio');
-
-               }  );
-
-
-               //------------configuracion del sonido ---------------
-              
-
-
-               this.starbutton   =  this.add.image(50,50,'musicaoff').setScale(0.05).setInteractive();
-            
-               this.starbutton.on('pointerdown', () =>{
-
-                // -----------------sonido-------------
-               this.add.image(50,50,'musicaon').setScale(0.05)
-              
-               
-
-
-                this.sonido=  this.sound.add('musicafondo');
-                const soundConfig = {
-                volumen :1 ,              // toda esta parte es del
-        
-                loop : true               // sonidoooo
-               }
-               // esto no va this.sonido.play(soundConfig);
-               // con esto solo carga una unica vez
-                if (!this.sound.locked){
-               // already unlocked so play
-                this.sonido.play(soundConfig)
-               }
-               else {
-                  //wait for 'unloched ' to free and the play 
-                this.sound.once(Phaser.Sound.Events.UNLOCKED, ()=>{
-                 this.sonido.play(soundConfig) 
-         
-                   } )
-                }
-
-                
- 
-                }  );
- 
-      }
-    //update
-    
   }
-   
-   export default Menu;  
+
+  preload() {
+    // en priload vamos a cargar nuestras imagenes que vamos a usar 
+    // para tenerlo disponibles para que la paguna luego la pueda renderizar
+    this.load.audio('musicafondo', '/public/img/musicafondo.mp3');
+    this.load.image('start', '/public/img/start.png');
+    this.load.image('menunave', '/public/img/fondomenunave.jpg');
+    this.load.image('titulo', '/public/img/titulo.png');
+    this.load.image('musicaon', '/public/img/botonsonidoactivo.png');
+    this.load.image('musicaoff', '/public/img/botonsonidodesactivado.png');
+
+
+
+  }
+
+  create() {
+    ////////////////////////////////////////////////////////
+    this.add.image(400, 300, 'menunave');  // imagen del fondo 
+    this.add.image(400, 200, 'titulo').setScale(0.4);
+
+
+    this.starbutton = this.add.image(400, 500, 'start').setScale(0.7).setInteractive();
+
+    this.starbutton.on('pointerdown', () => {
+      this.scene.start('inicio');
+
+    });
+
+
+    //------------configuracion del sonido ---------------
+
+
+
+    this.starbutton = this.add.image(50, 50, 'musicaoff').setScale(0.05).setInteractive();
+
+    this.starbutton.on('pointerdown', () => {
+
+      // -----------------sonido-------------
+      this.add.image(50, 50, 'musicaon').setScale(0.05)
+
+
+
+
+      this.sonido = this.sound.add('musicafondo');
+      const soundConfig = {
+        volumen: 1,              // toda esta parte es del
+
+        loop: true               // sonidoooo
+      }
+      // esto no va this.sonido.play(soundConfig);
+      // con esto solo carga una unica vez
+      if (!this.sound.locked) {
+        // already unlocked so play
+        this.sonido.play(soundConfig)
+      }
+      else {
+        //wait for 'unloched ' to free and the play 
+        this.sound.once(Phaser.Sound.Events.UNLOCKED, () => {
+          this.sonido.play(soundConfig)
+
+        })
+      }
+
+
+
+    });
+
+  }
+  //update
+
+}
+
+export default Menu;  
--- src/escenas/inicio.js
+++ src/escenas/inicio.js
@@ -1,86 +1,89 @@
-class inicio extends Phaser.Scene{
-    constructor(){
-        super("inicio");
-        // aqui tambien se puede agregar variables
-
-        
-      }
- 
-      preload (){
-
-        // es un metodo
-        //this.load.setBaseURL('https://labs.phaser.io');
-       
-        this.load.image('fondo', '/public/img/fondo.png'); // esta es la imagen de fondo
-        this.load.spritesheet('portal','/public/img/portalDefinido.png',{frameWidth:162.25,frameHeight:384});
-        this.load.spritesheet('bala','/public/img/bala.png',{frameWidth:72,frameHeight:72});
-        this.load.spritesheet('nave','/public/img/nave.png', {frameWidth:70, frameHeight:62}); // la nave principal
-        this.load.image('red', '/public/img/red.png');
-
-      }
-      create (){
-        this.balas = this.physics.add.group(); // Crea un grupo para las balas
-        this.barraespacio = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.K); // Configura la tecla espaciadoraaa
+class inicio extends Phaser.Scene {
+  constructor() {
+    super("inicio");
+    // aqui tambien se puede agregar variables
 
 
-        this.add.image(400,400, 'fondo'); 
-        this.portal = this.physics.add.sprite(700,300, 'portal');
-      
+  }
+
+  preload() {
+
+    // es un metodo
+    //this.load.setBaseURL('https://labs.phaser.io');
+
+    this.load.image('fondo1', '/public/img/fondo.png'); // esta es la imagen de fondo
+    this.load.spritesheet('portal', '/public/img/portalDefinido.png', { frameWidth: 162.25, frameHeight: 384 });
+    this.load.spritesheet('bala', '/public/img/bala.png', { frameWidth: 72, frameHeight: 72 });
+    this.load.spritesheet('nave', '/public/img/nave.png', { frameWidth: 70, frameHeight: 62 }); // la nave principal
+    this.load.image('red', '/public/img/red.png');
+
+  }
+  create() {
+    this.balas = this.physics.add.group(); // Crea un grupo para las balas
+    this.barraespacio = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.K); // Configura la tecla espaciadoraaa
+    this.add.image(400, 400, 'fondo1');
+
+    this.portal = this.physics.add.sprite(700, 300, 'portal');
 
 
 
-        this.player = this.physics.add.sprite(400,400, 'nave');
-        this.player.setCollideWorldBounds(true); //no atravesar bordes del area de juego
-        
-          // son las particulas 
-          const particles = this.add.particles(-40,0, 'red', {
-            speed: 100,
-            angle:{min: 150, max :210},
-            scale: { start: 1.0, end: 0 },  // EL TAMAÑO DE LAS PARTICULAS """"""""""""""""""""""""""""""""""""
-            blendMode: 'ADD' // este agraga las particulas 
-  
-        });
-  
-        particles.startFollow(this.player); 
-        
-      
-        this.cursors = this.input.keyboard.createCursorKeys();
 
-        this.anims.create({
-          //key: 'right',
-          frames: this.anims.generateFrameNumbers('portal', { start: 0, end: 3 }),
-          frameRate: 10,
-          repeat: -1
-      });
+    this.player = this.physics.add.sprite(400, 400, 'nave');
+    this.player.setCollideWorldBounds(true); //no atravesar bordes del area de juego
+
+    // son las particulas 
+    const particles = this.add.particles(-40, 0, 'red', {
+      speed: 100,
+      angle: { min: 150, max: 210 },
+      scale: { start: 1.0, end: 0 },  // EL TAMAÑO DE LAS PARTICULAS """"""""""""""""""""""""""""""""""""
+      blendMode: 'ADD' // este agraga las particulas 
+
+    });
+
+    particles.startFollow(this.player);
 
 
-        this.anims.create({
-          key: 'left',
-          frames: this.anims.generateFrameNumbers('nave', { start: 0, end: 0 }),
-          //[{key: 'avion', frame:2  }],//('avion',/*{start:0 , end:0 }*/)
-          //frameRate: 30
-          //repeat: -1 // valor negativo para repeticion infinita, 0 para una sola
-                      //reproduccion, 1 para dos repeticiones, y asi sucesivamente
-      });
+    this.cursors = this.input.keyboard.createCursorKeys();
 
-      this.anims.create({
-          key: 'up',
-          frames: this.anims.generateFrameNumbers('nave', { start: 2, end: 2 }),
-          //frameRate: 20
-      });
+    this.anims.create({
+      key: 'animacionPortal',
+      frames: this.anims.generateFrameNumbers('portal', { start: 0, end: 3 }),
+      frameRate: 6,
+      repeat: -1
+    });
+    this.portal.play('animacionPortal')
+    this.physics.add.overlap(this.player,this.portal, ()=>{
+      this.scene.start('Escena1')
+    })
 
-      this.anims.create({
-          key: 'right',
-          frames: this.anims.generateFrameNumbers('nave', { start: 0, end: 0 }),
-          //frameRate: 10
-          ///repeat: -1
-      });
 
-      this.anims.create({
-        key: 'down',
-        frames: this.anims.generateFrameNumbers('nave', { start: 1, end: 1 }),
-        //frameRate: 10
-        //repeat: -1
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('nave', { start: 0, end: 0 }),
+      //[{key: 'avion', frame:2  }],//('avion',/*{start:0 , end:0 }*/)
+      //frameRate: 30
+      //repeat: -1 // valor negativo para repeticion infinita, 0 para una sola
+      //reproduccion, 1 para dos repeticiones, y asi sucesivamente
+    });
+
+    this.anims.create({
+      key: 'up',
+      frames: this.anims.generateFrameNumbers('nave', { start: 2, end: 2 }),
+      //frameRate: 20
+    });
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('nave', { start: 0, end: 0 }),
+      //frameRate: 10
+      ///repeat: -1
+    });
+
+    this.anims.create({
+      key: 'down',
+      frames: this.anims.generateFrameNumbers('nave', { start: 1, end: 1 }),
+      //frameRate: 10
+      //repeat: -1
     });
     ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -89,57 +92,57 @@ class inicio extends Phaser.Scene{
     // colision con el portal -- this.physics.add.collider(this.player, this.portal, this.pasarnivel, null, this);
 
 
-      }
-      
+  }
 
 
 
-      update(){   
 
-        if (this.cursors.left.isDown) {
-          this.player.setVelocityX(-160);
-          this.player.anims.play('left', true);
-      }else if(this.cursors.right.isDown){
-          this.player.setVelocityX(160); 
-          this.player.anims.play('right', true);
-      }
+  update() {
+
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+      this.player.anims.play('left', true);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+      this.player.anims.play('right', true);
+    }
 
 
-      else if(this.cursors.up.isDown){
-        this.player.setVelocityY(-160);
-        this.player.setVelocityX(0);
+    else if (this.cursors.up.isDown) {
+      this.player.setVelocityY(-160);
+      this.player.setVelocityX(0);
 
-        
-        this.player.anims.play('up', true);
-       }
 
-    else if(this.cursors.down.isDown){
+      this.player.anims.play('up', true);
+    }
+
+    else if (this.cursors.down.isDown) {
       this.player.setVelocityY(160);
       this.player.setVelocityX(0);
-      
+
       this.player.anims.play('down', true);
-  }
-      else{
-          this.player.setVelocityX(0);
-          this.player.setVelocityY(0);
-          
-          this.player.anims.play('left', true);
-      }
-
-
-      if (Phaser.Input.Keyboard.JustDown(this.barraespacio)) { // Verifica si la tecla espaciadora se presionó en este fotograma
-        const bullet = this.balas.create(this.player.x,this.player.y, 'bala'); // Crea una bala en la posición del jugador
-        bullet.setVelocity(300, 0); // Establece la velocidad de la bala (en este caso, hacia arriba)
     }
-      }
-/* pasar al nivel con el portal
-      pasarnivel(player, portal){
-         
-       // player.setTint(0xff0000);
-        // player.anims.play('turn');
-          this.scene.start('Escena1');
-}
-*/
+    else {
+      this.player.setVelocityX(0);
+      this.player.setVelocityY(0);
+
+      this.player.anims.play('left', true);
+    }
+
+
+    if (Phaser.Input.Keyboard.JustDown(this.barraespacio)) { // Verifica si la tecla espaciadora se presionó en este fotograma
+      const bullet = this.balas.create(this.player.x, this.player.y, 'bala'); // Crea una bala en la posición del jugador
+      bullet.setVelocity(300, 0); // Establece la velocidad de la bala (en este caso, hacia arriba)
+    }
+  }
+  /* pasar al nivel con el portal
+        pasarnivel(player, portal){
+           
+         // player.setTint(0xff0000);
+          // player.anims.play('turn');
+            this.scene.start('Escena1');
+  }
+  */
 }
 
-export  default inicio;
+export default inicio;
